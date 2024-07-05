c3b0e2496a694f8d63264d97903c72d5498e0735
nzvyagina18 <nzvyagina@mail.ru>
2024-07-05 16:14:52 +0300

    Move AB_test to fixture

--- AB_test1.py
+++ AB_test1.py
@@ -1,83 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from group import Group
-
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        options = Options()
-        options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-        self.wd = webdriver.Firefox(executable_path=r'C:\Users\nadya\PycharmProjects\repo1\geckodriver.exe', options=options)
-        self.wd.implicitly_wait(30)
-
-    def test_create_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, "admin", "secret")
-        self.create_group(wd, Group("aaa", "bbb", "ccc"))
-        self.logout(wd)
-
-    def test_create_empty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, "admin", "secret")
-        self.create_group(wd, Group("", "", ""))
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def create_group(self, wd, group):
-        # Open Groups page
-        wd.find_element_by_id("container").click()
-        wd.find_element_by_link_text("groups").click()
-        # initiate group creation
-        wd.find_element_by_name("new").click()
-        # fill in group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit form
-        wd.find_element_by_name("submit").click()
-        # return to groups page
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, wd, user, password):
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(user)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def tearDown(self):
-        self.wd.quit()
+from address_book_lib import AddressBook
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def a_b(request):
+    fixture = AddressBook()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_create_group(a_b):
+    a_b.login( "admin", "secret")
+    a_b.create_group(Group("aaa", "bbb", "ccc"))
+    a_b.logout()
+
+
+def test_create_empty_group(a_b):
+    a_b.login("admin", "secret")
+    a_b.create_group(Group("", "", ""))
+    a_b.logout()
--- address_book_lib.py
+++ address_book_lib.py
@@ -4,32 +4,74 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 
 
-def run_address_book(self):
-    options = Options()
-    options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-    self.wd = webdriver.Firefox(executable_path=r'C:\Users\nadya\PycharmProjects\repo1\geckodriver.exe', options=options)
-    self.wd.implicitly_wait(30)
+class AddressBook:
+    def __init__(self):
+        options = Options()
+        options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+        self.wd = webdriver.Firefox(executable_path=r'C:\Users\nadya\PycharmProjects\repo1\geckodriver.exe',
+                                        options=options)
+        self.wd.implicitly_wait(30)
 
-
-def OpenHomePage(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/")
 
+    def login(self, user, password):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(user)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
 
-def is_element_present(self, how, what):
-    try:
-        self.wd.find_element(by=how, value=what)
-    except NoSuchElementException as e:
-        return False
-    return True
+    def open_groups_page(self):
+        wd = self.wd
+        wd.find_element_by_id("container").click()
+        wd.find_element_by_link_text("groups").click()
 
+    def return_to_groups_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
 
-def is_alert_present(self):
-    try:
-        self.wd.switch_to_alert()
-    except NoAlertPresentException as e:
-        return False
-    return True
+    def logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
 
+    def create_group(self, group):
+        # Open Groups page
+        wd = self.wd
+        self.open_groups_page()
+        # initiate group creation
+        wd.find_element_by_name("new").click()
+        # fill in group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit form
+        wd.find_element_by_name("submit").click()
+        # return to groups page
+        self.return_to_groups_page()
 
-def tearDown(self):
-    self.wd.quit()
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def destroy(self):
+        self.wd.quit()
+
+    def is_alert_present(self):
+            try:
+                self.wd.switch_to_alert()
+            except NoAlertPresentException as e:
+                return False
+            return True
