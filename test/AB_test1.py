21b00d43c92996babec135354ddeb292f7c80318
nzvyagina18 <nzvyagina@mail.ru>
2024-07-09 13:17:56 +0300

    ContactHelper class is created.

--- test/AB_test1.py
+++ test/AB_test1.py
@@ -10,12 +10,10 @@ def a_b(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-
 def test_create_group(a_b):
     a_b.session.login( "admin", "secret")
     a_b.group.create(Group("aaa", "bbb", "ccc"))
     a_b.session.logout()
-
 
 def test_create_empty_group(a_b):
     a_b.session.login("admin", "secret")
--- test/contacts_test.py
+++ test/contacts_test.py
@@ -15,7 +15,7 @@ def a_b(request):
 
 def test_add_contact(a_b):
     a_b.session.login("admin", "secret")
-    a_b.add_contact(Contact("Contact1", "Contact1_nick", "Contact1_last", "Contact1_mid", "Contact1_title",
+    a_b.contact.add(Contact("Contact1", "Contact1_nick", "Contact1_last", "Contact1_mid", "Contact1_title",
                          "Contact1_company", "Contact1 address", "2222222", "+79212222222", "+79112222222", "2222222",
-                         "contact1@email1.ru", "contact1@email2.ru", "contact1@email3.ru", "http://mail.ru", ContactDate("11", "June", "2000"), ContactDate("11", "June", "2020") ), ContactPage())
+                         "contact1@email1.ru", "contact1@email2.ru", "contact1@email3.ru", "http://mail.ru", ContactDate("11", "June", "2000"), ContactDate("11", "June", "2020")), ContactPage())
     a_b.session.logout()
