ecb35bcea5a638353c1da3c7745a03da364fc225
nzvyagina18 <nzvyagina@mail.ru>
2024-07-13 23:29:39 +0300

    edit_contact, populate_contact_data methods added

--- fixture/contact.py
+++ fixture/contact.py
@@ -4,6 +4,11 @@ class ContactHelper:
     def __init__(self, ab):
         self.ab = ab
 
+    def open_contact_page(self):
+        wd = self.ab.wd
+        wd.find_element_by_id("container").click()
+        wd.find_element_by_link_text("home").click()
+
     def populate(self, fieldname, value):
         wd = self.ab.wd
         wd.find_element_by_name(fieldname).click()
@@ -26,9 +31,15 @@ class ContactHelper:
 
     def add(self, contact, contact_page):
         wd = self.ab.wd
+        self.open_contact_page()
         wd.find_element_by_id("container").click()
         wd.find_element_by_link_text("add new").click()
-        self.upload_photo(contact.photo) #"C:\\Users\\nadya\\desktop\\snegir.png"
+        self.populate_contact_data(contact, contact_page)
+        # submit form
+        wd.find_element_by_name("submit").click()
+
+    def populate_contact_data(self, contact, contact_page):
+        self.upload_photo(contact.photo)
         self.populate(contact_page.firstname, contact.firstname)
         self.populate(contact_page.midname, contact.midname)
         self.populate(contact_page.lastname, contact.lastname)
@@ -46,47 +57,31 @@ class ContactHelper:
         self.populate(contact_page.homepage, contact.homepage)
         self.set_date(contact_page.birthdate, contact.birthdate)
         self.set_date(contact_page.ann_date, contact.ann_date)
-        # submit form
-        wd.find_element_by_name("submit").click()
 
     def edit(self, contact_name, contact_new, contact_page):
         wd = self.ab.wd
-        checkbox = '//input [contains(@title,"' + contact_name + '")]'
-        contact_id = wd.find_element_by_xpath(checkbox).get_attribute("id")
-        edit_button = '//a[@href = "edit.php?id=' + contact_id + '"]/img'
-        wd.find_element_by_xpath(edit_button).click()
-        self.upload_photo(contact_new.photo)
-        self.populate(contact_page.firstname, contact_new.firstname)
-        self.populate(contact_page.midname, contact_new.midname)
-        self.populate(contact_page.lastname, contact_new.lastname)
-        self.populate(contact_page.nickname, contact_new.nickname)
-        self.populate(contact_page.title, contact_new.title)
-        self.populate(contact_page.company, contact_new.company)
-        self.populate(contact_page.address, contact_new.address)
-        self.populate(contact_page.homephone, contact_new.homephone)
-        self.populate(contact_page.mobilephone, contact_new.mobilephone)
-        self.populate(contact_page.workphone, contact_new.workphone)
-        self.populate(contact_page.fax, contact_new.fax)
-        self.populate(contact_page.email1, contact_new.email1)
-        self.populate(contact_page.email2, contact_new.email2)
-        self.populate(contact_page.email3, contact_new.email3)
-        self.populate(contact_page.homepage, contact_new.homepage)
-        self.set_date(contact_page.birthdate, contact_new.birthdate)
-        self.set_date(contact_page.ann_date, contact_new.ann_date)
+        self.open_contact_page()
+        self.edit_contact(contact_name, wd)
+        self.populate_contact_data(contact_new, contact_page)
         # update form
         wd.find_element_by_name("update").click()
 
-    def delete_from_edit(self, contact_name):
-        wd = self.ab.wd
+    def edit_contact(self, contact_name, wd):
         checkbox = '//input [contains(@title,"' + contact_name + '")]'
         contact_id = wd.find_element_by_xpath(checkbox).get_attribute("id")
         edit_button = '//a[@href = "edit.php?id=' + contact_id + '"]/img'
         wd.find_element_by_xpath(edit_button).click()
+
+    def delete_from_edit(self, contact_name):
+        wd = self.ab.wd
+        self.open_contact_page()
+        self.edit_contact(contact_name, wd)
         # delete contact
         wd.find_element_by_xpath('//input[@value = "Delete"]').click()
 
     def delete_from_home(self, contact_name):
         wd = self.ab.wd
+        self.open_contact_page()
         checkbox = '//input [contains(@title,"' + contact_name + '")]'
         wd.find_element_by_xpath(checkbox).click()
         wd.find_element_by_xpath('//input[@value = "Delete"]').click()
