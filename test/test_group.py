b1969624360d31f6150c280bda26af7099012540
nzvyagina18 <nzvyagina@mail.ru>
2024-07-22 22:33:16 +0300

    test create group - verification added

--- test/test_group.py
+++ test/test_group.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+from sys import maxsize
 
 
 def test_create_group(a_b):
     old_groups = a_b.group.get_group_list()
-    a_b.group.create(Group("b1", "bbb", "ccc"))
+    group = Group("b1", "bbb", "ccc")
+    a_b.group.create(group)
     new_groups = a_b.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
 
 
 def test_create_empty_group(a_b):
     old_groups = a_b.group.get_group_list()
-    a_b.group.create(Group("", "", ""))
+    group = Group("", "", "")
+    a_b.group.create(group)
     new_groups = a_b.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
