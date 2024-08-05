8778e1a317b033b7e60cea6e2ec20e29b27c7ce0
nzvyagina18 <nzvyagina@mail.ru>
2024-08-05 22:52:01 +0300

    run parameters added

--- fixture/address_book_lib.py
+++ fixture/address_book_lib.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import IEDriverManager
+#from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
@@ -8,20 +11,37 @@ from fixture.contact import ContactHelper
 
 
 class AddressBook:
-    def __init__(self):
+    def __init__(self, browser, base_url, login, password):
+
         options = Options()
         options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-        self.wd = webdriver.Firefox(executable_path=r'geckodriver.exe',
-                                    options=options)
-        self.wd.implicitly_wait(10)
+        #self.wd = webdriver.Firefox(executable_path=r'geckodriver.exe',
+        #                            options=options)
+        #self.wd.implicitly_wait(10)
         #self.wd.WebDriver() #this code does not work for me
+        #self.session = SessionHelper(self)
+        #self.group = GroupHelper(self)
+        #self.contact = ContactHelper(self)
+
+        if browser == 'firefox':
+            self.wd = webdriver.Firefox(executable_path=r'geckodriver.exe',
+                                        options=options)
+        elif browser == 'chrome':
+            self.wd = webdriver.Chrome(ChromeDriverManager().install())
+        elif browser == 'ie':
+            self.wd = webdriver.Ie(IEDriverManager().install())
+        else:
+            raise ValueError("Unrecognized browser %s", browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
+        self.login = login
+        self.password = password
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
--- test/conftest.py
+++ test/conftest.py
@@ -7,12 +7,16 @@ fixture = None
 @pytest.fixture
 def a_b(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseURL")
+    login = request.config.getoption("--login")
+    password = request.config.getoption("--password")
     if fixture is None:
-        fixture = AddressBook()
+        fixture = AddressBook(browser = browser, base_url = base_url, login = login, password = password)
     else:
         if not fixture.is_valid():
-            fixture = AddressBook()
-    fixture.session.ensure_login("admin", "secret")
+            fixture = AddressBook(browser = browser, base_url = base_url, login = login, password = password)
+    fixture.session.ensure_login(login, password)
     return fixture
 
 
@@ -23,4 +27,11 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action = 'store', default = 'firefox')
+    parser.addoption("--baseURL", action='store', default="http://localhost/addressbook/")
+    parser.addoption("--login", action='store')
+    parser.addoption("--password", action='store')
 
