6d874811b8b1f38149d5901ea7234fbc3104fae3
nzvyagina18 <nzvyagina@mail.ru>
2024-08-27 17:41:25 +0300

    ORM is implemented for groups and contacts

--- fixture/orm.py
+++ fixture/orm.py
@@ -1,4 +1,6 @@
 from pony.orm import *
+from model.group import Group
+from model.contact import Contact
 
 class ORMFixture:
 
@@ -14,13 +16,28 @@ class ORMFixture:
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
         id = PrimaryKey(int, column='id')
-        firstname = Optional(str, column = 'firstname')
-        lastname = Optional(str, column = 'lastname')
+        firstname = Optional(str, column='firstname')
+        lastname = Optional(str, column='lastname')
 
     def __init__(self, host, name, user, password):
         self.db.bind('mysql', host=host, database=name, user=user, password=password)
         self.db.generate_mapping()
+        sql_debug(True)
+
+    def convert_groups_to_model(self, groups):
+        def convert(group):
+            return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
+        return list(map(convert, groups))
 
     @db_session
     def get_group_list(self):
-        return list(select(g for g in ORMFixture.ORMGroup))
+        return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
+
+    def convert_contacts_to_model(self, contacts):
+        def convert(contact):
+            return Contact(id=str(contact.id), firstname=contact.firstname, lastname=contact.lastname)
+        return list(map(convert, contacts))
+
+    @db_session
+    def get_contact_list(self):
+        return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact))
